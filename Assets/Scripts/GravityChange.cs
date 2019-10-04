ebc7a4d71d83067b8df86ddc450998c1defd1d97
ManuelMeekattukulam <manuel.meekattukulam@gmail.com>
2019-10-04 16:44:05 -0400

    Space, earth, moon gravity working

--- Assets/Scripts/GravityChange.cs
+++ Assets/Scripts/GravityChange.cs
@@ -6,9 +6,19 @@ public class GravityChange : MonoBehaviour
 {
     void OnTriggerEnter(Collider collider)
     {
-        if (collider.gameObject.tag == "Button")
+        if (collider.gameObject.tag == "Button1")
         {
             Physics.gravity = new Vector3(0f, 0f, 0f);
+        }
+
+        if (collider.gameObject.tag == "Button2")
+        {
+            Physics.gravity = new Vector3(0f, -1.62f, 0f);
+        }
+
+        if (collider.gameObject.tag == "Button3")
+        {
+            Physics.gravity = new Vector3(0f, -9.81f, 0f);
         }
     }
 }
