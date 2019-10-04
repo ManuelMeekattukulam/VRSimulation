62fed368a335f1db052af81a2447b401489fd9b2
ManuelMeekattukulam <manuel.meekattukulam@gmail.com>
2019-10-04 16:29:16 -0400

    gravity script cleaned

--- Assets/Scripts/GravityChange.cs
+++ Assets/Scripts/GravityChange.cs
@@ -8,25 +8,7 @@ public class GravityChange : MonoBehaviour
     {
         if (collider.gameObject.tag == "Button")
         {
-            //Physics.gravity.y = 0;
-
-            //float g = 0;
-            //Physics gamePhysics;
-            //gamePhysics = 1;
-           
-
-            //float y = Physics.gravity.y;
-
-            //Vector3 
-
-
-            //Physics.bounceThreshold = 1;
             Physics.gravity = new Vector3(0f, 0f, 0f);
-
-
-
-
-
         }
     }
 }
