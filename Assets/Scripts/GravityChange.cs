3b12a70cd70efbd490fb209f9f130099f9b9d5e0
ManuelMeekattukulam <manuel.meekattukulam@gmail.com>
2019-10-04 13:48:35 -0400

    gravity simulation script added

---
+++ Assets/Scripts/GravityChange.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityChange : MonoBehaviour
+{
+    [SerializeField]
+    private Transform boxPrefab;
+    [SerializeField]
+    private Transform spawnPoint;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Button")
+        {
+            Transform spawnPosition = Instantiate(boxPrefab);
+            spawnPosition.position = spawnPoint.position;
+        }
+    }
+}
